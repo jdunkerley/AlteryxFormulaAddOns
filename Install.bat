55f4c7584b8094029f1762e01a2eb4f26576ba96
James Dunkerley <jdunkerley@users.noreply.github.com>
2016-04-07 23:56:44 +0100

    Update Install.bat
    Merge better install from Alteryx AddIns

--- Install.bat
+++ Install.bat
@@ -1,24 +1,43 @@
 @echo off
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
+
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-REM --> If error flag set, we do not have admin.
+
 if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
+    if '%1' NEQ 'ELEV' (
+        echo Requesting administrative privileges...
+        goto UACPrompt
+    ) else (
+        exit /B -1
+    )
+) else (
+    goto gotAdmin
+)
+
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "ELEV", "", "runas", 1 >> "%temp%\getadmin.vbs"
     "%temp%\getadmin.vbs"
     exit /B
+
 :gotAdmin
-	pushd "%~dp0" 
-	:: 	xcopy StringUtils.xml "%ProgramFiles%\Alteryx\bin\RuntimeData\FormulaAddIn\"
-	xcopy DateUtils.xml "%ProgramFiles%\Alteryx\bin\RuntimeData\FormulaAddIn\"
-    xcopy MathUtils.xml "%ProgramFiles%\Alteryx\bin\RuntimeData\FormulaAddIn\"
-    xcopy Random.xml "%ProgramFiles%\Alteryx\bin\RuntimeData\FormulaAddIn\"
-	popd
-	pause
-:-------------------------------------- 
+    FOR /F "usebackq tokens=2,* skip=2" %%L IN (
+        `reg query "HKLM\SOFTWARE\WOW6432Node\SRC\Alteryx" /v InstallDir64`
+    ) DO SET alteryxPath=%%M
+
+    pushd "%~dp0"
+
+    powershell -Command "gci . | Unblock-File"
+
+    if "%alteryxPath%" NEQ "" (
+:: 	xcopy StringUtils.xml "%ProgramFiles%\Alteryx\bin\RuntimeData\FormulaAddIn\"
+	xcopy DateUtils.xml "%alteryxPath%\RuntimeData\FormulaAddIn\"
+        xcopy MathUtils.xml "%alteryxPath%\RuntimeData\FormulaAddIn\"
+        xcopy Random.xml "%alteryxPath%\RuntimeData\FormulaAddIn\"
+        echo FormulaAddIns installed to %alteryxPath%\RuntimeData\FormulaAddIn\
+    ) else (
+        echo Please copy "%cd$\*.xml" to <AlteryxInstallDir>\RuntimeData\FormulaAddIn\
+    )
+
+    popd
+    pause
+    
