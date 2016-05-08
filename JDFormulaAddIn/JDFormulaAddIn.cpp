2c27bbf0996431a4196e0a616fb958f5589f78b7
James Dunkerley <james.dunkerley@gmail.com>
2016-05-08 22:53:21 +0100

    Moving more to C++
    HexBins in C++
    Split function
    Some more test workflows

--- JDFormulaAddIn/JDFormulaAddIn.cpp
+++ JDFormulaAddIn/JDFormulaAddIn.cpp
@@ -1,4 +1,4 @@
-// AlteryxAddIn.cpp : Defines the exported functions for the DLL application.
+﻿// AlteryxAddIn.cpp : Defines the exported functions for the DLL application.
 //
 #include "stdafx.h"
 #include "JDFormulaAddIn.h"
@@ -7,7 +7,7 @@ void SetString(FormulaAddInData *pReturnValue, const wchar_t *pString)
 {
 	size_t nLen = wcslen(pString);
 	wchar_t *pStringRet = (wchar_t *)GlobalAlloc(GMEM_FIXED, (nLen + 1) * sizeof(wchar_t));
-	wcscpy_s(pStringRet, nLen, pString);
+	wcscpy(pStringRet, pString);
 	pReturnValue->pVal = pStringRet;
 	pReturnValue->nVarType = 2;
 }
@@ -20,7 +20,7 @@ void ResetIsNull(int nNumArgs, FormulaAddInData *pArgs)
 	}
 }
 
-// this sample takes a variable number of numbers and simply sums them together, returning a number
+// this sample takes a variable number of inputs and returns the first non-null
 extern "C" long _declspec(dllexport) _stdcall Coalesce(int nNumArgs, FormulaAddInData *pArgs, FormulaAddInData *pReturnValue)
 {
 	pReturnValue->nVarType = pArgs[0].nVarType;
@@ -43,5 +43,152 @@ extern "C" long _declspec(dllexport) _stdcall Coalesce(int nNumArgs, FormulaAddI
 	}
 
 	ResetIsNull(nNumArgs, pArgs);
+	return 1;
+}
+
+// Need String, String (char), Integer
+extern "C" long _declspec(dllexport) _stdcall Split(int nNumArgs, FormulaAddInData *pArgs, FormulaAddInData *pReturnValue)
+{
+	pReturnValue->nVarType = 2;
+
+	// Check Input Parameters
+	if (nNumArgs != 3 ||
+		pArgs[0].isNull || pArgs[0].nVarType != 2 ||
+		pArgs[1].isNull || pArgs[1].nVarType != 2 ||
+		pArgs[2].isNull || pArgs[2].nVarType != 1 || pArgs[2].dVal < 0) {
+		pReturnValue->isNull = 1;
+		ResetIsNull(nNumArgs, pArgs);
+		return 1;
+	}
+
+	// Copy the string
+	size_t nLen = wcslen(pArgs[0].pVal);
+	if (nLen == 0) {
+		pReturnValue->isNull = true;
+		return 1;
+	}
+
+	wchar_t *input = new wchar_t[nLen + 1];
+	wcscpy(input, pArgs[0].pVal);
+
+	// Split the String
+	wchar_t* buffer = NULL;
+	wchar_t *token = NULL;
+	token = wcstok_s(input, pArgs[1].pVal, &buffer);
+	int count = 1;
+	while (token != NULL && count < pArgs[2].dVal) {
+		token = wcstok_s(NULL, pArgs[1].pVal, &buffer);
+		count++;
+	}
+
+	if (token == NULL) {
+		pReturnValue->isNull = true;
+	} else {
+		SetString(pReturnValue, token);
+	}
+
+	delete input;
+	return 1;
+}
+
+// Need Double X, Double Y, Double R
+extern "C" long _declspec(dllexport) _stdcall HexBinX(int nNumArgs, FormulaAddInData *pArgs, FormulaAddInData *pReturnValue)
+{
+	pReturnValue->nVarType = 1;
+
+	// Check Input Parameters
+	if (nNumArgs < 2 || nNumArgs > 3 ||
+		pArgs[0].isNull || pArgs[0].nVarType != 1 ||
+		pArgs[1].isNull || pArgs[1].nVarType != 1 ||
+		(nNumArgs == 3 && pArgs[2].nVarType != 1)) {
+		pReturnValue->isNull = 1;
+		ResetIsNull(nNumArgs, pArgs);
+		return 1;
+	}
+
+	double r = (pArgs[2].isNull ? 1.0 : pArgs[2].dVal);
+	double dy = 2 * 0.86602540378443864676372317075294 * r; // 2 * Sin(π/3)
+	double dx = 1.5 * r;
+
+	double px = pArgs[0].dVal / dx;
+	int pi = (int)round(px);
+	bool mod2 = (pi & 1) == 1;
+	double py = pArgs[1].dVal / dy - (mod2 ? 0.5 : 0);
+	double pj = round(py);
+	double px1 = (px - pi) * dx;
+
+	if (fabs(px1) * 3 > 1)
+	{
+		double py1 = (py - pj) * dy;
+		double pj2 = pj + (py < pj ? -1 : 1) / 2.0;
+		int pi2 = pi + (px < pi ? -1 : 1);
+		double px2 = (px - pi2) * dx;
+		double py2 = (py - pj2) * dy;
+
+		if (px1 * px1 + py1 * py1 > px2 * px2 + py2 * py2)
+		{
+			pi = pi2;
+		}
+	}
+
+	if (isnan(pi * dx)) {
+		pReturnValue->isNull = 1;
+	}
+	else {
+		pReturnValue->dVal = pi * dx;
+	}
+
+	return 1;
+}
+
+// Need Double X, Double Y, Double R
+extern "C" long _declspec(dllexport) _stdcall HexBinY(int nNumArgs, FormulaAddInData *pArgs, FormulaAddInData *pReturnValue)
+{
+	pReturnValue->nVarType = 1;
+
+	// Check Input Parameters
+	if (nNumArgs < 2 || nNumArgs > 3 ||
+		pArgs[0].isNull || pArgs[0].nVarType != 1 ||
+		pArgs[1].isNull || pArgs[1].nVarType != 1 ||
+		(nNumArgs == 3 && pArgs[2].nVarType != 1)) {
+		pReturnValue->isNull = 1;
+		ResetIsNull(nNumArgs, pArgs);
+		return 1;
+	}
+
+	double r = (pArgs[2].isNull ? 1.0 : pArgs[2].dVal);
+	double dy = 2 * 0.86602540378443864676372317075294 * r; // 2 * Sin(π/3)
+	double dx = 1.5 * r;
+
+	double px = pArgs[0].dVal / dx;
+	int pi = (int)round(px);
+	bool mod2 = (pi & 1) == 1;
+	double py = pArgs[1].dVal / dy - (mod2 ? 0.5 : 0);
+	double pj = round(py);
+	double px1 = (px - pi) * dx;
+
+	if (fabs(px1) * 3 > 1)
+	{
+		double py1 = (py - pj) * dy;
+		double pj2 = pj + (py < pj ? -1 : 1) / 2.0;
+		int pi2 = pi + (px < pi ? -1 : 1);
+		double px2 = (px - pi2) * dx;
+		double py2 = (py - pj2) * dy;
+
+		if (px1 * px1 + py1 * py1 > px2 * px2 + py2 * py2)
+		{
+			pj = pj2 + (mod2 ? 1 : -1) / 2.0;
+			pi = pi2;
+			mod2 = (pi & 1) == 1;
+		}
+	}
+
+	if (isnan((pj + (mod2 ? 0.5 : 0)) * dy)) {
+		pReturnValue->isNull = 1;
+	}
+	else {
+		pReturnValue->dVal = (pj + (mod2 ? 0.5 : 0)) * dy;
+	}
+
 	return 1;
 }
--- JDFormulaAddIn/stdafx.h
+++ JDFormulaAddIn/stdafx.h
@@ -6,6 +6,7 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
+#include <math.h>
 
 
 // TODO: reference additional headers your program requires here
