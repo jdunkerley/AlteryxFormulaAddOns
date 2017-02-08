c27cc30de6b99972bcd9bb0e786b21af14b98198
James Dunkerley <james.dunkerley@gmail.com>
2017-02-08 21:41:46 +0000

    New release script and DateFromDMY and DateFromMDY

---
+++ CreateRelease.ps1
@@ -0,0 +1,9 @@
+﻿$version = "1.3"
+
+$root = Split-Path -Parent $PSCommandPath
+
+$output = "$root\AlteryxAbacus $version.zip"
+Compress-Archive -Path ("$root\*Utils.xml", "$root\*.dll", "*nstall.bat") -DestinationPath $output -Verbose -Update
+
+$output = "$root\AlteryxAbacus Tests $version.zip"
+Compress-Archive -Path ("$root\*.Test", "$root\*.yxm*") -DestinationPath $output -Verbose -Update
