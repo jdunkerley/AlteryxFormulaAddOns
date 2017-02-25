83c13d1db3ee8d9a9c1e7f7c41141fb4b427bdd1
James Dunkerley <james.dunkerley@gmail.com>
2017-02-25 22:35:50 +0000

    Improved Installers

--- Install.bat
+++ Install.bat
@@ -1,16 +1,29 @@
 @echo off
 
+pushd "%~dp0"
+powershell -Command "gci . | Unblock-File"
+
 FOR /F "usebackq tokens=2,* skip=2" %%L IN (
     `reg query "HKLM\SOFTWARE\WOW6432Node\SRC\Alteryx" /v InstallDir64`
 ) DO SET alteryxPath=%%M
 
 if "%alteryxPath%" NEQ "" (
-    pushd "%~dp0"
-    powershell -Command "gci . | Unblock-File"
     .\InstallAlteryxAbacus.exe "%~dp0\*.dll" "%~dp0\*.xml" "%alteryxPath%\RuntimeData\FormulaAddIn"
     echo FormulaAddIns installed to %alteryxPath%\RuntimeData\FormulaAddIn\
-    popd
-) else (
-    echo Please copy "%~dp0*.xml" to $AlteryxInstallDir$\RuntimeData\FormulaAddIn\
+)
+
+FOR /F "usebackq tokens=2,* skip=2" %%L IN (
+    `reg query "HKCU\SOFTWARE\SRC\Alteryx" /v InstallDir64`
+) DO SET alteryxUserPath=%%M
+
+if "%alteryxUserPath%" NEQ "" (
+    .\InstallAlteryxAbacus.exe "%~dp0\*.dll" "%~dp0\*.xml" "%alteryxUserPath%\RuntimeData\FormulaAddIn"
+    echo FormulaAddIns installed to %alteryxUserPath%\RuntimeData\FormulaAddIn\
+)
+
+if "%alteryxUserPath%%alteryxPath%" EQU "" (
+    echo Please copy all xml and dll files from "%~dp0"  to $AlteryxInstallDir$\RuntimeData\FormulaAddIn\
     pause
 )
+
+popd
