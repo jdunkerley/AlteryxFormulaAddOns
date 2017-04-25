bdb7ceaeeed41e435f6a12263cec1fa6243c7f86
James Dunkerley <james.dunkerley@gmail.com>
2017-04-25 07:39:48 +0100

    Version changes

--- InstallAndRunUnitTests.ps1
+++ InstallAndRunUnitTests.ps1
@@ -4,13 +4,13 @@ Push-Location $root
 $bins = @()
 
 Write-Host "Finding Alteryx Admin Install Location..."
-$reg = Get-ItemProperty HKLM:\SOFTWARE\WOW6432Node\SRC\AlteryxZ -ErrorAction SilentlyContinue
+$reg = Get-ItemProperty HKLM:\SOFTWARE\WOW6432Node\SRC\Alteryx -ErrorAction SilentlyContinue
 if ($reg -ne $null) {
     $bins += $reg.InstallDir64
 }
 
 Write-Host "Finding Alteryx User Install Location..."
-$reg = Get-ItemProperty HKCU:\SOFTWARE\SRC\AlteryxZ -ErrorAction SilentlyContinue
+$reg = Get-ItemProperty HKCU:\SOFTWARE\SRC\Alteryx -ErrorAction SilentlyContinue
 if ($reg -ne $null) {
     $bins += $reg.InstallDir64
 }
