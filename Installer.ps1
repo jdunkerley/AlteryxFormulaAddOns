4b0bb1e9cc9e10d5578ee8bad8fc3ca47cfd7f8f
James Dunkerley <jdunkerley@users.noreply.github.com>
2017-09-22 21:42:18 +0100

    Fix if InstallDir64 not there

--- Installer.ps1
+++ Installer.ps1
@@ -8,13 +8,13 @@ $bins = @()
 
 Write-Host "Finding Alteryx Admin Install Location..."
 $reg = Get-ItemProperty HKLM:\SOFTWARE\WOW6432Node\SRC\Alteryx -ErrorAction SilentlyContinue
-if ($reg -ne $null) {
+if ($reg -ne $null -and $reg.InstallDir64 -ne $null) {
     $bins += $reg.InstallDir64
 }
 
 Write-Host "Finding Alteryx User Install Location..."
 $reg = Get-ItemProperty HKCU:\SOFTWARE\SRC\Alteryx -ErrorAction SilentlyContinue
-if ($reg -ne $null) {
+if ($reg -ne $null -and $reg.InstallDir64 -ne $null) {
     $bins += $reg.InstallDir64
 }
 
