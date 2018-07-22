332ad7d5a2c9860c20cd0047145fe3b9cf003789
James Dunkerley <jdunkerley@scottlogic.com>
2018-07-22 22:02:07 +0100

    Overhaul Random IP

--- AlteryxAbacus/Generator.cpp
+++ AlteryxAbacus/Generator.cpp
@@ -24,32 +24,6 @@ static auto getCharacter(wchar_t const* input, const size_t length)
 	return input[getUniformInt(length - 1)];
 }
 
-static std::vector<std::wstring> &splitStrW(const std::wstring &InputString, wchar_t delimiterChar, std::vector<std::wstring> &ResultVec) {
-	std::wstringstream sStream(InputString);
-	std::wstring item;
-
-	while (std::getline(sStream, item, delimiterChar)) {
-		ResultVec.push_back(item);
-	}
-
-	return ResultVec;
-}
-
-static int parseInt(const std::wstring &InputString)
-{
-	std::size_t index = 0;
-	try {
-		const auto result = std::stoi(InputString, &index);
-		if (index == InputString.length()) {
-			return result;
-		}
-	} catch(...)
-	{		
-	}
-
-	return -1;
-}
-
 static void createFromStandard(wchar_t const* pattern, const size_t length, wchar_t * output)
 {
 	static const wchar_t* upper = L"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -82,10 +56,10 @@ static void createFromStandard(wchar_t const* pattern, const size_t length, wcha
 
 static void createFromCharset(wchar_t const* pattern, const size_t length, std::vector<const wchar_t*> const& charsets, std::vector<size_t> const& lengths, wchar_t * output)
 {
-	const int setscount = charsets.size();
+	const auto setscount = charsets.size();
 
 	for (auto i = 0LLU; i < length; i++) {
-		int index = -1;
+		auto index = -1;
 		if (pattern[i] >= L'0' && pattern[i] <= L'9')
 		{
 			index = static_cast<int>(pattern[i] - L'0');
@@ -181,55 +155,49 @@ extern "C" long _declspec(dllexport) _stdcall RandomIPAddress(int nNumArgs, Form
 		return AlteryxAbacusUtils::ReturnError(L"Arguments: [CIDR]", pReturnValue, nNumArgs, pArgs);
 	}
 
-	if (pArgs[0].isNull)
+	if (nNumArgs == 1 && pArgs[0].isNull)
 	{
 		pReturnValue->isNull = true;
 		return AlteryxAbacusUtils::ReturnSuccess(nNumArgs, pArgs);
 	}
 
-	const std::wstring CIDR = nNumArgs == 1 ? std::wstring(pArgs[0].pVal) : L"0.0.0.0/0";
-	std::vector<std::wstring> cidr_parts;
-	splitStrW(CIDR, L'/', cidr_parts);
-
-	if (cidr_parts.size() != 2)
+	std::array<int, 5> cidr_parts{};
+	if (nNumArgs == 0)
 	{
-		return AlteryxAbacusUtils::ReturnError(L"Invalid CIDR passed", pReturnValue, nNumArgs, pArgs);
+		cidr_parts[0] = 0;
+		cidr_parts[1] = 0;
+		cidr_parts[2] = 0;
+		cidr_parts[3] = 0;
+		cidr_parts[4] = 0;
 	}
-
-	int range;
-	if ((range = parseInt(cidr_parts[1])) < 0 || range > 32)
+	else
 	{
-		return AlteryxAbacusUtils::ReturnError(L"Invalid CIDR mask value passed", pReturnValue, nNumArgs, pArgs);
-	}
-
-	std::vector<std::wstring> ip_parts;
-	splitStrW(cidr_parts[0], L'.', ip_parts);
-	std::array<int, 4> ip_numbers{};
-	if (ip_parts.size() != 4 ||
-		(ip_numbers[0] = parseInt(ip_parts[0])) < 0 || ip_numbers[0] > 255 ||
-		(ip_numbers[1] = parseInt(ip_parts[1])) < 0 || ip_numbers[0] > 255 ||
-		(ip_numbers[2] = parseInt(ip_parts[2])) < 0 || ip_numbers[0] > 255 ||
-		(ip_numbers[3] = parseInt(ip_parts[3])) < 0 || ip_numbers[0] > 255
-		)
-	{
-		return AlteryxAbacusUtils::ReturnError(L"Invalid IP Address passed", pReturnValue, nNumArgs, pArgs);
-	}
-
-	std::wstringstream output;
-	for (auto i = 0; i < 4; i++)
-	{
-		const int mask = getUniformInt((1 << std::max<int>(8 - range, 0)) - 1);
-		range = max(0, range - 8);
-
-		output << ip_numbers[i] + mask;
-
-		if (i < 3)
+		const auto cidr = pArgs[0].pVal;
+		wchar_t *end;
+		if (cidr[0] < L'0' || cidr[0] > L'9' || (cidr_parts[0] = wcstoul(cidr, &end, 10)) > 255 || *end != L'.' ||
+			end[1] < L'0' || end[1] > L'9' || (cidr_parts[1] = wcstoul(end + 1, &end, 10)) > 255 || *end != L'.' ||
+			end[1] < L'0' || end[1] > L'9' || (cidr_parts[2] = wcstoul(end + 1, &end, 10)) > 255 || *end != L'.' ||
+			end[1] < L'0' || end[1] > L'9' || (cidr_parts[3] = wcstoul(end + 1, &end, 10)) > 255 || *end != L'/' ||
+			end[1] < L'0' || end[1] > L'9' || (cidr_parts[4] = wcstoul(end + 1, &end, 10)) > 32 || *end != L'\0')
 		{
-			output << L'.';
+			return AlteryxAbacusUtils::ReturnError(L"Invalid CIDR passed", pReturnValue, nNumArgs, pArgs);
 		}
 	}
 
-	AlteryxAbacusUtils::SetString(pReturnValue, output.str().c_str());
+	auto range = cidr_parts[4];
+	auto *pStringRet = static_cast<wchar_t *>(GlobalAlloc(GMEM_FIXED, 16 * sizeof(wchar_t)));
+	wchar_t *end = pStringRet;
+	for (auto i = 0; i < 4; i++)
+	{
+		const int ip = cidr_parts[i] + getUniformInt((1 << std::max<int>(8 - range, 0)) - 1);
+		range = max(0, range - 8);
+		_itow(ip, end, 10);
+		end += (ip < 10 ? 2 : (ip < 100 ? 3 : 4));
+		*(end - 1) = L'.';
+	}
+	*(end - 1) = L'\0';
+
+	pReturnValue->pVal = pStringRet;
 	return AlteryxAbacusUtils::ReturnSuccess(nNumArgs, pArgs);
 }
 
